c6fe13217e35252c031c6f05b1d7b7e7011982f4
juandaf <juandaforer@gmail.com>
2023-02-14 11:52:47 -0500

    Cargando archivo index con el contenido requerido

---
+++ index.js
@@ -0,0 +1,12 @@
+const obj = {
+    nombre: "Juan David",
+    Edad: 28,
+    Eres_desarrollador: true,
+    Fecha_nacimiento: new Date(1994,2,8),
+    Librofav: {
+        titulo: "Ser Feliz en Alaska", 
+        autor: "Rafael Santandreu ",
+        fecha: new Date(2016,0.1),
+        url: null
+    }
+}
